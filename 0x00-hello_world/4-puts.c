e45f24234e770646c9af136528cc78cf722a5c7d
Israel <israeladdo5@gmail.com>
2023-02-16 11:25:44 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Main function
+ *
+ * Return: always 0
+
+
+
+int main()
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+
+
+}
