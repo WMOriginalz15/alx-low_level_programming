2070a61761fbacd79cb09be8e0a0f6d06fff84b6
Israel <israeladdo5@gmail.com>
2023-02-17 20:22:30 -0800

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 /**
- *  * main - main function
- *   *
- *    * Return: always 0
- *     */
+ * main - main function
+ *
+ * Return: always 0
+ */
 
 int main(void)
 {
-	        char i;
-		        for  (i = 'a' ; i <= 'z' ; i++)
-				                if (i != 'q' && i != 'e')
-							                        putchar(i);
-			        putchar('\n');
-				        return (0);
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		if (i != 'q' && i != 'e')
+			putchar(i);
+	putchar('\n');
+	return (0);
 }
